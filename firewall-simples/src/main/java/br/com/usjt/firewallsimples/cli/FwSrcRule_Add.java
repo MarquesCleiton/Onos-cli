9147030bb42d162f6200cfdd8e27724b3c95e816
marquescleiton <cleiton.contato2210@gmail.com>
2020-10-26 18:38:04 -0300

    teste Cli

---
+++ firewall-simples/src/main/java/br/com/usjt/firewallsimples/cli/FwSrcRule_Add.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2014 Open Networking Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package br.com.usjt.firewallsimples.cli;
+
+import org.apache.karaf.shell.commands.Command;
+import org.onosproject.cli.AbstractShellCommand;
+
+@Command(scope = "proactiveFirewall", name = "fwAdd-srcRule", description = "Adicione Regra de Bloqueio para a origem especificada - IP/[PORTA]")
+public class FwSrcRule_Add extends AbstractShellCommand{
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
